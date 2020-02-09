d23f078a090f528b17c722677246bc4e5ae5fc25
Keun Kyeong <woodeekim@gmail.com>
2020-02-09 19:21:19 +0900

    PostsAPiControllerTest 까지 완료

--- src/main/java/com/woodee/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/woodee/book/springboot/domain/posts/PostsRepository.java
@@ -2,5 +2,5 @@ package com.woodee.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostsRepository extends JpaRepository<Posts,Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
