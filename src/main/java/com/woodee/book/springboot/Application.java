f6e663af28acc8abcb4a7951b25bd5c7b023f742
Keun Kyeong <woodeekim@gmail.com>
2020-02-09 19:01:32 +0900

    HelloControllerTest 까지 잘됨

---
+++ src/main/java/com/woodee/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.woodee.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/woodee/book/springboot/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.woodee.book.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
