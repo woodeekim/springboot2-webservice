12265d1e1cd4c8409b57ad8d2fdee1a52dd3ce92
Keun Kyeong <woodeekim@gmail.com>
2020-02-10 20:12:48 +0900

    UPDATE/DELETE 기능 구현

--- src/main/java/com/woodee/book/springboot/web/IndexController.java
+++ src/main/java/com/woodee/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.woodee.book.springboot.web;
 
 import com.woodee.book.springboot.service.PostsService;
+import com.woodee.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,5 +27,13 @@ public class IndexController {
         return "posts-save";
     }
 
+    //글 수정
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+    }
 
 }
