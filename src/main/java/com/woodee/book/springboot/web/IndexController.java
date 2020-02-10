f5c2018b75f98717cf4cece323469f5f328d3069
Keun Kyeong <woodeekim@gmail.com>
2020-02-11 07:06:33 +0900

    알 수 없는 에러

--- src/main/java/com/woodee/book/springboot/web/IndexController.java
+++ src/main/java/com/woodee/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.woodee.book.springboot.web;
 
+import com.woodee.book.springboot.config.auth.dto.SessionUser;
+import com.woodee.book.springboot.domain.user.User;
 import com.woodee.book.springboot.service.PostsService;
 import com.woodee.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     //글 리스트 조회
     @GetMapping("/")
     public String index(Model model){
+
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
