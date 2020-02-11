db253569c3dd0cf5b6defc87b7b01c91eccd2b10
Keun Kyeong <woodeekim@gmail.com>
2020-02-11 22:26:01 +0900

    Fix login to add login annotation

---
+++ src/main/java/com/woodee/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,4 @@
+package com.woodee.book.springboot.config.auth;
+
+public class LoginUserArgumentResolver {
+}
--- src/main/java/com/woodee/book/springboot/web/IndexController.java
+++ src/main/java/com/woodee/book/springboot/web/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
