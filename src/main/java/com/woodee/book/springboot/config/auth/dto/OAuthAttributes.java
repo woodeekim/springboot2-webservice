48e3519c11c11fe013f69c3790d471bffdffde78
Keun Kyeong <woodeekim@gmail.com>
2020-02-12 17:39:23 +0900

    Implement loginNaver

--- src/main/java/com/woodee/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/woodee/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,8 +24,23 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
 
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAtrributeName, Map<String, Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAtrributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -37,6 +52,7 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
 
 
     public User toEntity() {
