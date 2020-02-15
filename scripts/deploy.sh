32d57165a5d6d5a5e3e99d110de56e5ddd761218
Keun Kyeong <woodeekim@gmail.com>
2020-02-16 00:56:16 +0900

    Update deploy.sh for RDS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -43,5 +43,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
- $REPOSITORY/$JAR_NAME 2>&1 &
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        $REPOSITORY/$JAR_NAME 2>&1 &
