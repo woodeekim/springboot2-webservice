8b8b13844fe851ddc0a12ed9f7f87084c8b0e148
Keun Kyeong <woodeekim@gmail.com>
2020-02-15 22:33:32 +0900

    Add deploy.sh for external security

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+c2-user@springboot2-webservice step1]$ vim deploy.sh
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
+    $REPOSITORY/$JAR_NAME 2>&1 &
