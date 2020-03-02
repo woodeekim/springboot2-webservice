1307f509fd1a3666416aab66d648d07b02927f54
Keun Kyeong <woodeekim@gmail.com>
2020-03-03 01:48:23 +0900

    Update deploy.sh, travies.yml and appspec.yml for /CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot2-webservice
 
 cd $REPOSITORY/$PROJECT_NAME/
 
-echo ">Git Pull"
-
-git pull
-
-echo ">프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> step1 디렉토리로 이동"
-
-cd $REPOSITORY
-
 echo "Build 파일 복사"
-
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+CURRENT_PID=$(pgrep -fl springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -42,7 +28,14 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
