ba052b862b7373856005ab1aec7e3226da92abf5
Keun Kyeong <woodeekim@gmail.com>
2020-03-07 05:08:26 +0900

    Hotfix failed test

--- src/test/java/com/woodee/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/woodee/book/springboot/web/IndexControllerTest.java
@@ -22,6 +22,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+        assertThat(body).contains("김근경");
     }
 }
